fdd1a58a0c0b3daea53d607d0a4efda8aea56943
Denver Stove <dtstove@gmail.com>
2018-10-31 09:54:08 +1100

    Implemented multiple MVC for Cassini

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -27,7 +27,6 @@ class ImageViewController: UIViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageURL = DemoURL.uwa
     }
     
     override func viewWillAppear(_ animated: Bool) {
